ffe6c861c4303d85b2f4976477ecd89904f02c09
mehedihasan1684 <88764299+mehedihasan1684@users.noreply.github.com>
2022-06-18 12:47:18 +0600

    first commit

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+ use App\User;
+  
+
+class MyController extends Controller{
+    
+    public function My(){
+
+        $myArray=array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+        return response()->json ($myArray);
+    }
+
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$router->get('/', 'MyController@My');
+
+
